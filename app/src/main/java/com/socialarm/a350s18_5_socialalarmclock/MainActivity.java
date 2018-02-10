e895dc925aad819bdbb8bfb2fd7980f3c893f895
Henry Zhu <henryzhu@seas.upenn.edu>
2018-02-10 14:31:19 -0500

    Added tabs to activity_main.xml

--- app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/MainActivity.java
+++ app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/MainActivity.java
@@ -1,7 +1,9 @@
 package com.socialarm.a350s18_5_socialalarmclock;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SetupTabs();
+    }
+
+
+    private void SetupTabs() {
+        TabHost top_tab = (TabHost)findViewById(R.id.top_tab);
     }
 }
