6d502b20bf2ebfb0537a75628c57832b9e88c26a
Alexander Perry <alexpe@seas.upenn.edu>
2018-03-14 15:39:59 -0400

    Alarm params (#11)
    * Create new activity for Alarm events.
    WIP as cannot cancel alarms. Cannot set multiple alarms.
    Snooze dismiss work.
    
    * Add more parameters to alarms
    
    * Fix main_activity sizing.
    
    * Add day of week to alarms
    
    * Add better retriggering for alarms that are dismissed.
    Disabling alarms is still broken.

--- app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/SingleAlarmAdapter.java
+++ app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/SingleAlarmAdapter.java
@@ -14,6 +14,8 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class SingleAlarmAdapter extends CursorAdapter {
@@ -68,21 +70,15 @@ public class SingleAlarmAdapter extends CursorAdapter {
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 AlarmsOpenHelper dbHelper = new AlarmsOpenHelper(internal_context);
                 if (b) {
-                    //LocalTime trigger_time = LocalTime.of(a.getHour(),a.getMinute());
-                    Calendar trigger_time = Calendar.getInstance();
-                    trigger_time.set(Calendar.HOUR_OF_DAY, hour);
-                    trigger_time.set(Calendar.MINUTE, minute);
-                    trigger_time.set(Calendar.SECOND, 0);
-                    Calendar now = Calendar.getInstance();
-                    if (trigger_time.before(now)) {
-                        trigger_time.add(Calendar.DAY_OF_MONTH, 1); // Set for tomorrow
-                    }
+                    Calendar trigger_time = getNextTrigger(hour, minute, days_of_week);
+
                     long tTime = trigger_time.getTimeInMillis();
                     Intent intent = new Intent(internal_context, AlarmEvent.class);
                     intent.putExtra("Alarm", id);
                     PendingIntent Alarm = PendingIntent.getActivity(internal_context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
                     AlarmManager manager = (AlarmManager) internal_context.getSystemService(Context.ALARM_SERVICE);
-                    manager.setExact(AlarmManager.RTC_WAKEUP, tTime, Alarm);
+                    //manager.setExact(AlarmManager.RTC_WAKEUP, tTime, Alarm);
+                    manager.setAlarmClock(new AlarmManager.AlarmClockInfo(tTime, Alarm), Alarm);
                     dbHelper.setActive(id, true);
                 } else {
                     Intent intent = new Intent(internal_context, AlarmEvent.class);
@@ -92,5 +88,49 @@ public class SingleAlarmAdapter extends CursorAdapter {
                 }
             }
         });
+    }
+
+    static Calendar getNextTrigger(int hour, int minute, int days_of_week) {
+        Set<Integer> calendar_days = new HashSet<Integer>();
+        if ((days_of_week & AlarmsOpenHelper.SUNDAY) != 0) {
+            calendar_days.add(Calendar.SUNDAY);
+        }
+        if ((days_of_week & AlarmsOpenHelper.MONDAY) != 0) {
+            calendar_days.add(Calendar.MONDAY);
+        }
+        if ((days_of_week & AlarmsOpenHelper.TUESDAY) != 0) {
+            calendar_days.add(Calendar.TUESDAY);
+        }
+        if ((days_of_week & AlarmsOpenHelper.WEDNESDAY) != 0) {
+            calendar_days.add(Calendar.WEDNESDAY);
+        }
+        if ((days_of_week & AlarmsOpenHelper.THURSDAY) != 0) {
+            calendar_days.add(Calendar.THURSDAY);
+        }
+        if ((days_of_week & AlarmsOpenHelper.FRIDAY) != 0) {
+            calendar_days.add(Calendar.FRIDAY);
+        }
+        if ((days_of_week & AlarmsOpenHelper.SATURDAY) != 0) {
+            calendar_days.add(Calendar.SATURDAY);
+        }
+
+        Calendar trigger_time = Calendar.getInstance();
+        trigger_time.set(Calendar.HOUR_OF_DAY, hour);
+        trigger_time.set(Calendar.MINUTE, minute);
+        trigger_time.set(Calendar.SECOND, 0);
+        Calendar now = Calendar.getInstance();
+        if (trigger_time.before(now)) {
+            trigger_time.add(Calendar.DAY_OF_MONTH, 1); // Set for tomorrow
+        }
+
+        for (int i = 0; i < 7; ++i) {
+            if (!calendar_days.contains(trigger_time.get(Calendar.DAY_OF_WEEK))) {
+                trigger_time.add(Calendar.DAY_OF_MONTH, 1);
+            } else {
+                break;
+            }
+        }
+
+        return trigger_time;
     }
 }
