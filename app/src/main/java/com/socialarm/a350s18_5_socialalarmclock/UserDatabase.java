cb90a3d753db81ffe06ce671dd6dd8d55a4d39f6
drewsb <drewboyette@gmail.com>
2018-03-14 15:25:10 -0400

    Moving checkUserExists method

--- app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/UserDatabase.java
+++ app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/UserDatabase.java
@@ -1,7 +1,12 @@
 package com.socialarm.a350s18_5_socialalarmclock;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
@@ -13,6 +18,23 @@ public class UserDatabase {
     public static final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     private static final String TAG = "UserDatabase";
+
+    public static void checkUserExists(final User user){
+        final DocumentReference docRef = db.collection("users").document(user.getId());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if(!document.exists()) {
+                        UserDatabase.addUser(user);
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
 
     public static void addUser(User user){
         // Add a new document with a generated ID
