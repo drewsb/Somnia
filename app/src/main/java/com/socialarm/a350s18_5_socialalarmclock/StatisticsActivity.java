e0cf32900c3a188f347b220b747d36eae75a0ac7
maknee <henryzhu@seas.upenn.edu>
2018-03-14 15:45:01 -0400

    removed strings generated by default activity and moved DownloadImageTask to its own class

--- app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/StatisticsActivity.java
+++ app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/StatisticsActivity.java
@@ -1,20 +1,11 @@
 package com.socialarm.a350s18_5_socialalarmclock;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.io.InputStream;
 
 public class StatisticsActivity extends AppCompatActivity {
 
@@ -49,36 +40,6 @@ public class StatisticsActivity extends AppCompatActivity {
         statisticsView.append("Wake up time: " + "" +"\n");
         statisticsView.append("Wake up time: " + "" +"\n");
 
-        
-
     }
 
-    //(FIX, THIS IS IN MAINACTIIVITY)
-    /*
-    DownloadImageTask is a private class used to convert URL's into a Bitmap asynchronously
- */
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
-    }
 }
