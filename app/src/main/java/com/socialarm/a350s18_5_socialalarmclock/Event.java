2de02a84b936e9c8570351f5837ad3a8484080c7
Henry Zhu <henryzhu@seas.upenn.edu>
2018-04-03 21:56:29 -0400

    Removed setters for event

--- app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/Event.java
+++ app/src/main/java/com/socialarm/a350s18_5_socialalarmclock/Event.java
@@ -26,39 +26,19 @@ public class Event {
         return action;
     }
 
-    public void setAction(String action) {
-        this.action = action;
-    }
-
     public String getAlarm_id() {
         return alarm_id;
-    }
-
-    public void setAlarm_id(String alarm_id) {
-        this.alarm_id = alarm_id;
     }
 
     public String getUser_id() {
         return user_id;
     }
 
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
-    }
-
     public String getEvent_id() {
         return event_id;
     }
 
-    public void setEvent_id(String event_id) {
-        this.event_id = event_id;
-    }
-
     public long getTimestamp() {
         return timestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
     }
 }
